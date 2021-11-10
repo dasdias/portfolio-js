25483cf5e4dcd62eb84594ee554788d697139f10
[Alexander Dianov] <dasdias@yandex.ru>
2021-11-10 22:16:29 +0300

    Добавлены скрипты к бургер меню, убран скролл при открытии меню и зафиксированна позиция окна

--- js/script.js
+++ js/script.js
@@ -1,78 +1,90 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const presentOrderBtn = document.querySelector('.present__order-btn');
-    const pageOverlayModal = document.querySelector('.page__overlay_modal');
-    const modalClose = document.querySelector('.modal__close');
+    const disableScroll = () => {
 
-
-    // const animateModal = (sk) => {
-    //     const speed = {
-    //         slow: 10,
-    //         medium: 5,
-    //         fast: 1,
-    //         default: 3,
-    //     };
-    //     if (opacity <= 0) {
-    //         const timer = setInterval(() => {
-    //                 opacity += 0.02;
-    //                 modal.style.opacity = opacity;
-    //                 if (opacity >= 1) clearInterval(timer);
-    //             }, speed[sk]);
-            
-    //     } else {
-    //         const timer = setInterval(() => {
-    //             opacity -= 0.02;
-    //             modal.style.opacity = opacity;
-    //             if (opacity <= 0) {
-    //                 clearInterval(timer);
-    //                 modal.classList.remove('page__overlay_modal_open');
-    //             }
-    //         }, speed[sk]);
-    //     }
-    // }
+        const widthScroll = window.innerWidth - document.body.offsetWidth;
+            document.body.scrollPosition = window.scrollY;
+            document.body.style.cssText = `
+            overflow: hidden;
+            position: fixed;
+            top: -${document.body.scrollPosition}px;
+            left: 0;
+            heigth: 100vh;
+            padding-right: ${widthScroll}px;
+        `;
+    }
+    const enableScroll = () => {
+        document.body.style.cssText = 'position: relative;';
+        window.scroll({top: document.body.scrollPosition})
+    }
     
-    const hendlerModal = function (openBtn, modal, openSelector, closeTrigger, sk = "medium") {
-        let opacity = 0;
-        const speed = {
-            slow: 10,
-            medium: 5,
-            fast: 1,
-            default: 3,
+    
+    {
+        const presentOrderBtn = document.querySelector('.present__order-btn');
+        const pageOverlayModal = document.querySelector('.page__overlay_modal');
+        const modalClose = document.querySelector('.modal__close');
+
+
+        const hendlerModal = function (openBtn, modal, openSelector, closeTrigger, sk = "medium") {
+            let opacity = 0;
+            const speed = {
+                slow: 10,
+                medium: 5,
+                fast: 1,
+                default: 3,
+            };
+            const openModal = () => {
+                disableScroll();
+                modal.style.opacity = opacity;
+                modal.classList.add(openSelector);
+                const timer = setInterval(() => {
+                    opacity += 0.02;
+                    modal.style.opacity = opacity;
+                    if (opacity >= 1) clearInterval(timer);
+                }, speed[sk] ? speed[sk] : speed.default);
+            }
+            const closeModal = () => {
+                enableScroll();
+                const timer = setInterval(() => {
+                    opacity -= 0.02;
+                    modal.style.opacity = opacity;
+                    if (opacity <= 0) {
+                        clearInterval(timer);
+                        modal.classList.remove('page__overlay_modal_open');
+                    }
+                }, speed[sk] ? speed[sk] : speed.default);
+            }
+            openBtn.addEventListener('click', openModal);
+            closeTrigger.addEventListener('click', closeModal);
+
+            modal.addEventListener('click', (e) => {
+                const target = e.target;
+                if (target === modal) {
+                    closeModal();
+                }
+                // if (target.classList.contains("page__overlay_modal")) {
+                //     closeModal();
+                // }
+            });
         };
 
-        openBtn.addEventListener('click', ()=> {
-            modal.style.opacity = opacity;
-            modal.classList.add(openSelector);
-            const timer = setInterval(() => {
-                opacity += 0.02;
-                modal.style.opacity = opacity;
-                if (opacity >= 1) clearInterval(timer);
-            }, speed[sk] ? speed[sk] : speed.default);
-        });
-        
-        closeTrigger.addEventListener('click', () => {
-            const timer = setInterval(() => {
-                opacity -= 0.02;
-                modal.style.opacity = opacity;
-                if (opacity <= 0) {
-                    clearInterval(timer);
-                    modal.classList.remove('page__overlay_modal_open');
-                }
-            }, speed[sk] ? speed[sk] : speed.default);
-        });
-        pageOverlayModal.addEventListener('click', (e) => {
-            const target = e.target;
-            if (target.classList.contains("page__overlay_modal")) {
-                const timer = setInterval(() => {
-                opacity -= 0.02;
-                modal.style.opacity = opacity;
-                if (opacity <= 0) {
-                    clearInterval(timer);
-                    modal.classList.remove('page__overlay_modal_open');
-                }
-            }, speed[sk] ? speed[sk] : speed.default);
-            }
-        });
-    };
+        hendlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'medium');
+    }
+    {
+        const headerContacts = document.querySelector('.header__contacts');
+        const headerContactsBurger = document.querySelector('.header__contacts-burger');
 
-    hendlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'medium');
+        const heandlerBurger = (openBtn, menu, openSelector) => {
+            openBtn.addEventListener('click', () => {
+                if (menu.classList.contains(openSelector)) {
+                    menu.style.height = '';
+                    menu.classList.remove(openSelector);
+                } else {
+                    menu.style.height = menu.scrollHeight + 'px';
+                    menu.classList.add(openSelector);
+
+                }
+            });
+        }
+        heandlerBurger(headerContactsBurger, headerContacts, 'header__contacts_open')
+    }
 });
